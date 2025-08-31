882ab7a8bf5a29b2d6f434c3826f5f31c6d46c9a
HarryTsai <sleepingturtle990866@gmail.com>
2025-08-31 10:12:00 +0800

    revise vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "bootstrap/scss/functions"; @import "bootstrap/scss/variables"; @import "bootstrap/scss/mixins";`
-      }
-    }
-  }
-})
+	// base 的寫法：
+	// base: '/Repository 的名稱/'
+	//這裡記得要改成 /QUANTIS-frontend/
+	base: "/QUANTIS-frontend/",
+	plugins: [react()],
+	server: {
+		port: 3000,
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		sourcemap: false,
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "bootstrap/scss/functions"; @import "bootstrap/scss/variables"; @import "bootstrap/scss/mixins";`,
+			},
+		},
+	},
+});
